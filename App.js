513a2aa9816b0e7dea0c5e56e5cd393aa908aa08
plamen1982 <plamen.hristov@ymail.com>
2017-10-16 14:12:13 -0400

    Included fetch method

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text, View } from 'react-native';
+import { AppRegistry, Text, View, List, FlatList } from 'react-native';
 
-class Blink extends Component {
+export default class Blink extends Component {
   constructor(props) {
     super(props);
-    this.state = {showText: true};
+    this.state = {
+      showText: true,
+      data : []
+    };
 
     // Toggle the state every second
     setInterval(() => {
@@ -13,24 +16,47 @@ class Blink extends Component {
       });
     }, 1000);
   }
+  componentDidMount() {
+    this.makeRemoteRequest();
+  }
+  makeRemoteRequest = () => {
+    // const { page, seed } = this.state;
+    const url = 'https://jsonplaceholder.typicode.com/posts/1';
+    // this.setState({ loading: true });
 
+    fetch(url)
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          data: [...this.state.data, ...res]
+        })
+      })
+      .catch(error => {
+        this.setState({ error, loading: false });
+      });
+  };
   render() {
-    let display = this.state.showText ? this.props.text : ' ';
+    let ime = this.state.data
+    // let display = this.state.showText ? this.props.text : ' ';
     return (
-      <Text>{display}</Text>
+      
+      <Text>
+        {this.state.data}
+      </Text>
+  
     );
   }
 }
 
-export default class BlinkApp extends Component {
-  render() {
-    return (
-      <View>
-        <Blink text='I love to blink' />
-        <Blink text='Yes blinking is so great' />
-        <Blink text='Why did they ever take this out of HTML' />
-        <Blink text='Look at me look at me look at me' />
-      </View>
-    );
-  }
-}
+// export default class BlinkApp extends Component {
+//   render() {
+//     return (
+//       <View>
+//         {/* <Blink text='I love to blink' />
+//         <Blink text='Yes blinking is so great' />
+//         <Blink text='Why did they ever take this out of HTML' />
+//         <Blink text='Look at me look at me look at me' /> */}
+//       </View>
+//     );
+//   }
+// }
