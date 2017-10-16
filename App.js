4fa873ee901961f37d91f9165c481765b3fdcf26
Ivan Ivanov <ipivanov11@gmail.com>
2017-10-16 21:35:57 +0300

    binded vars from fetch and displayed

--- App.js
+++ App.js
@@ -1,62 +1,88 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text, View, List, FlatList } from 'react-native';
+import { 
+    AppRegistry, 
+    Text, 
+    View, 
+    List, 
+    FlatList, 
+    StyleSheet 
+} from 'react-native';
 
-export default class Blink extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showText: true,
-      data : []
-    };
+class Blink extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showText: true,
+            data: []
+        };
 
-    // Toggle the state every second
-    setInterval(() => {
-      this.setState(previousState => {
-        return { showText: !previousState.showText };
-      });
-    }, 1000);
-  }
-  componentDidMount() {
-    this.makeRemoteRequest();
-  }
-  makeRemoteRequest = () => {
-    // const { page, seed } = this.state;
-    const url = 'https://jsonplaceholder.typicode.com/posts/1';
-    // this.setState({ loading: true });
+        // Toggle the state every second
+        setInterval(() => {
+            this.setState(previousState => {
+                return { showText: !previousState.showText };
+            });
+        }, 1000);
+    }
 
-    fetch(url)
-      .then(res => res.json())
-      .then(res => {
-        this.setState({
-          data: [...this.state.data, ...res]
-        })
-      })
-      .catch(error => {
-        this.setState({ error, loading: false });
-      });
-  };
-  render() {
-    let ime = this.state.data
-    // let display = this.state.showText ? this.props.text : ' ';
-    return (
-      
-      <Text>
-        {this.state.data}
-      </Text>
-  
-    );
-  }
+    render() {
+        let display = this.state.showText ? this.props.text : ' ';
+        return (
+            <Text style={[styles.title]}> { display } </Text>
+        );
+    }
 }
 
-// export default class BlinkApp extends Component {
-//   render() {
-//     return (
-//       <View>
-//         {/* <Blink text='I love to blink' />
-//         <Blink text='Yes blinking is so great' />
-//         <Blink text='Why did they ever take this out of HTML' />
-//         <Blink text='Look at me look at me look at me' /> */}
-//       </View>
-//     );
-//   }
-// }
+export default class BlinkApp extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: {}
+        };
+    }
+    componentDidMount() {
+        this.makeRemoteRequest();
+    }
+
+    makeRemoteRequest = () => {
+        // const { page, seed } = this.state;
+        const url = 'https://jsonplaceholder.typicode.com/posts/1';
+        // this.setState({ loading: true });
+
+        fetch(url)
+            .then(res => res.json())
+            .then(res => {
+                this.setState({
+                    data: res
+                })
+            })
+            .catch(error => {
+                this.setState({ error, loading: false });
+            });
+    };
+    render() {
+    let title = this.state.data.title;
+    let body = this.state.data.body;
+    let id = this.state.data.userId;
+    return (
+        <View style={styles.container}>
+            <Blink text={`id: ${id}`} />
+            <Blink text={`title: ${title}`} />
+            <Blink text={`description: ${body}`} />
+        </View>
+    );
+    }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    borderRadius: 4,
+    borderWidth: 0.5,
+    borderColor: '#d6d7da',
+    padding: 15
+  },
+  title: {
+    fontSize: 19,
+    fontWeight: 'bold',
+    margin: 5
+  }
+});
