958df8938fab53cc2891e4940565e0a2c9df10fd
Ivan Ivanov <ipivanov11@gmail.com>
2017-10-29 14:47:36 +0200

    Added image to the day screen

--- app/screens/Day.js
+++ app/screens/Day.js
@@ -1,13 +1,15 @@
 import React, { Component, PropTypes } from 'react';
 import { FlatList } from 'react-native';
 import moment from 'moment';
-
+import { colors } from '../config/styles';
 import {
   Text,
   View,
   StyleSheet,
-  ScrollView
+  ScrollView,
+  Image
 } from 'react-native';
+import { signs } from '../config/signs';
 
 class Day extends Component {
   constructor(props) {
@@ -18,7 +20,10 @@ class Day extends Component {
     let data = this.props.screenProps[0];
     let dayInfo = data.day;
     let name = data.name;
-    var now = moment().format();
+    var now = moment().format('dddd (DD.MM.YYYY)');
+    const sign = signs.filter(function( obj ) {
+      return name === obj.name;
+    });
     return (
       <View style={{
         elevation: 1,
@@ -30,12 +35,15 @@ class Day extends Component {
         flexWrap: 'wrap',
         padding: 20,
         margin: 20,
-        marginTop: 20,
+        marginTop: 50,
         backgroundColor: '#fff',
       }}>
+        <Image style={{width: 70, height: 70}} source={sign[0].img}/>
         <Text style={{ fontFamily: 'Roboto', fontSize: 20, marginBottom: 10 }}>{name}</Text>
-        <Text>{now}</Text>
-        <Text style={{ fontFamily: 'Roboto', fontSize: 16 }}>{dayInfo}</Text>
+        <Text style={{ fontFamily: 'Roboto', fontSize: 14, marginBottom: 10, color: colors.dimmedText }}>{now}</Text>
+        <ScrollView>
+          <Text style={{ fontFamily: 'Roboto', fontSize: 16 }}>{dayInfo}</Text>
+        </ScrollView>
       </View>
     );
   }
